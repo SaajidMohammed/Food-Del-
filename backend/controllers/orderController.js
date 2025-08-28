79b60eeea10a50ccb2db86a4b0b405618e84b43e
SaajidMohammed <saajidmohammed6@gmail.com>
2025-08-28 20:21:03 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://food-del-frontend-app-lccj.onrender.com";
 
     try {
         const newOrder = new orderModel({
